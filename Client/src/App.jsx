2b448e5ba5316d850f32a44cf7dce3e17c61df70
Joshua Candia <joshuacandia74@gmail.com>
2023-09-27 15:11:08 -0300

    feat(Client/App):routes added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,50 @@
+import { useState } from 'react'
+import Loader from './components/Loader'
+import Create from './views/Create'
+import Submit from './views/Submit'
+import { Route, Routes } from 'react-router-dom'
 import Form from './components/Form'
+import Options from './components/Options'
+import Responses from './views/Responses'
+import Error404 from './views/404'
 
 function App() {
+   const [loading, setLoading] = useState(false)
    return (
-      <div className='flex flex-col justify-center items-center min-h-scree'>
-         <div className='h-[20vh] bg-[#FFFF01] w-full flex justify-center items-center'></div>
-         <Form />
-      </div>
+      <>
+         {loading ? (
+            <Loader />
+         ) : (
+            <div>
+               <Routes>
+                  <Route
+                     path='/'
+                     element={
+                        <Create>
+                           <Form setLoading={setLoading} />
+                        </Create>
+                     }
+                  />
+                  <Route
+                     path='/submit'
+                     element={
+                        <Submit>
+                           <Options setLoading={setLoading} />
+                        </Submit>
+                     }
+                  />
+                  <Route
+                     path='/responses'
+                     element={<Responses />}
+                  />
+                  <Route
+                     path='*'
+                     element={<Error404 />}
+                  />
+               </Routes>
+            </div>
+         )}
+      </>
    )
 }
 
