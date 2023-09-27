11e55be44a9c53cd0c54780bb32128d50103102c
Joshua Candia <joshuacandia74@gmail.com>
2023-09-27 17:17:24 -0300

    feat(Client/App):views added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,11 +5,21 @@ import Submit from './views/Submit'
 import { Route, Routes } from 'react-router-dom'
 import Form from './components/Form'
 import Options from './components/Options'
+import Cards from './components/Cards'
 import Responses from './views/Responses'
 import Error404 from './views/404'
 
 function App() {
+   const [survey, setSurvey] = useState({
+      full_name: '',
+      phone_number: '',
+      start_date: '',
+      preferred_language: '',
+      how_found: '',
+      newsletter_subscription: false
+   })
    const [loading, setLoading] = useState(false)
+   const [id, setId] = useState('')
    return (
       <>
          {loading ? (
@@ -21,7 +31,12 @@ function App() {
                      path='/'
                      element={
                         <Create>
-                           <Form setLoading={setLoading} />
+                           <Form
+                              survey={survey}
+                              setSurvey={setSurvey}
+                              setLoading={setLoading}
+                              setId={setId}
+                           />
                         </Create>
                      }
                   />
@@ -29,13 +44,24 @@ function App() {
                      path='/submit'
                      element={
                         <Submit>
-                           <Options setLoading={setLoading} />
+                           <Options
+                              setSurvey={setSurvey}
+                              setLoading={setLoading}
+                           />
                         </Submit>
                      }
                   />
                   <Route
                      path='/responses'
-                     element={<Responses />}
+                     element={
+                        <Responses
+                           setSurvey={setSurvey}
+                           setLoading={setLoading}
+                           id={id}
+                        >
+                           <Cards />
+                        </Responses>
+                     }
                   />
                   <Route
                      path='*'
